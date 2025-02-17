30a16f248bdb1f6bafedc7e89dc1a805c5cbe6a6
Ginorris <rizzutigino@gmail.com>
2025-02-17 07:00:46 -0300

    feat: integrate API fetching, concurrent photo downloads, and timing logs in main workflow

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Ginorris/homevision-backend/internal/api"
+	"github.com/Ginorris/homevision-backend/internal/config"
+	"github.com/Ginorris/homevision-backend/internal/downloader"
+	"github.com/Ginorris/homevision-backend/internal/models"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-    fmt.Println("Hello, World")
+	start := time.Now()
+	// Load environment variables from .env file.
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Warning: no .env file found")
+	}
+
+	// Load configuration from file and environment variables.
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Error loading configuration: %v", err)
+	}
+
+	fmt.Print(cfg)
+	
+	// TODO fecth pages concurrently to handle higher workloads
+	// TODO use pageCount as a parameter or amount of houses to fetch
+	// Fetch pages sequentially.
+	fetchStart := time.Now()
+	var houses []models.House
+	for i := 1; i <= cfg.PageCount; i++ {
+		page, err := api.FetchPage(i, cfg.MaxRetries, cfg.RetryDelay, cfg.APIURL)
+		if err != nil {
+			// If there's an error (e.g., JSON parsing), log it and continue.
+			log.Printf("Error fetching page %d: %v", i, err)
+			continue
+		}
+		// Log number of houses for this page.
+		fmt.Printf("Fetched %d houses from page %d\n", len(page.Houses), i)
+		houses = append(houses, page.Houses...)
+	}
+	fmt.Printf("Fetching took %s\n", time.Since(fetchStart))
+
+	// Download photos concurrently.
+	downloadStart := time.Now()
+	if err := downloader.DownloadPhotos(houses, cfg.Concurrency, cfg.MediaDir); err != nil {
+		log.Fatalf("Error downloading photos: %v", err)
+	}
+	fmt.Printf("Photo downloads took %s\n", time.Since(downloadStart))
+
+	fmt.Printf("Total processing time: %s\n", time.Since(start))
 }
